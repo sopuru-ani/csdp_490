d136eb01809a4ad528c11293b822de9b3ed49e78
sopuru-ani <sopuruani41@gmail.com>
2026-04-08 18:32:14 -0400

    lots of ui changes x3

--- src/Overview.jsx
+++ src/Overview.jsx
@@ -7,8 +7,8 @@ function Overview() {
     <>
       <div className="w-dvw min-h-dvh h-auto flex flex-row bg-primary-soft">
         <Sidebar />
-        <div className="flex-1 max-h-dvh flex justify-center items-center">
-          <div className="h-full overflow-y-auto w-[60%] flex justify-center">
+        <div className="flex-1 max-h-dvh">
+          <div className="h-full overflow-y-auto">
             <Outlet />
           </div>
         </div>
