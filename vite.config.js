9a62c0ffcc83596d8ab146b547dcdec3ce13185a
sopuru-ani <sopuruani41@gmail.com>
2026-04-12 21:43:37 -0400

    ui changes part 2x2

--- vite.config.js
+++ vite.config.js
@@ -31,13 +31,13 @@ export default defineConfig({
         scope: "/",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "/pwa-512x512.png",
+            src: "/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any"
